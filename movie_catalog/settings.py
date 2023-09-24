5757faa6250238c835158f8e5c574e9a607970e8
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-09-24 04:00:02 +0300

    added static files for pages app

--- movie_catalog/settings.py
+++ movie_catalog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles", # working with static files
 ]
 
 MIDDLEWARE = [
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/" # default definition of the path and folder name for static files 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
