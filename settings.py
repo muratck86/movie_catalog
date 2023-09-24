10b16998b4e68c511cdb7cb9bd14a472df28083f
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-09-24 04:09:41 +0300

    static files moved under root folder

--- settings.py
+++ settings.py
@@ -113,7 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/" # default definition of the path and folder name for static files 
+STATIC_URL = "static/" # default definition of the path and folder name for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "/var/www/static/"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
