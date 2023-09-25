d8cacede8ef270fd1bfee172215db2244c86a427
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-09-25 06:16:34 +0300

    listing and editing movies in the admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Movie
 
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "year", "duration", "created_date", "isReleased")
+    list_display_links = ("id", "name", "year", "duration", "created_date")
+    list_filter = ("year", "created_date")
+    search_fields = ("name", "description")
+    list_editable = ("isReleased",)
+    list_per_page = 20
+
+
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
--- movies/models.py
+++ movies/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100,  verbose_name="Movie Title")
+    description = models.TextField(verbose_name="Presentation")
     image = models.CharField(max_length=50)
-    year = models.IntegerField()
-    duration = models.IntegerField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    year = models.IntegerField(verbose_name="Release Year")
+    duration = models.IntegerField(verbose_name="Duration (mins)")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name="Date Added")
+    isReleased = models.BooleanField(default=True, verbose_name="Released")
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
