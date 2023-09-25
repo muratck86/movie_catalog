62eeb870cf5bba4d4a8ac8e7668e810187363730
Murat Can Kurt <kurt.muratcan@gmail.com>
2023-09-26 02:03:39 +0300

    humanize added.

--- movie_catalog/settings.py
+++ movie_catalog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles", # working with static files
+    "django.contrib.humanize" # converts texts to more readable and attractive. 
 ]
 
 MIDDLEWARE = [
